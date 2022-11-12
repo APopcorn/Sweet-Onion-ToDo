2478d028012365b2b4c9c6c1cd58b6df012a6845
Ernst Padron <97245274+APopcorn@users.noreply.github.com>
2022-11-12 11:32:27 +0100

    testing package file

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
 import ProjectWindow from "./components/projectWindow/ProjectWindow";
 import Menu from "./components/utils/menu/Menu";
 import Search from "./components/utils/Search";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        
-      </Routes>
-    </Router>
-      
-  );
+    return <ProjectWindow />;
 }
 
 export default App;
